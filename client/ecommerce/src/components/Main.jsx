57edbe747f161217c6bf11d10a5fc1509020a878
hackanonimous <esi.segurity@gmail.com>
2023-02-17 17:56:11 -0500

    archivo server para deploy

--- client/ecommerce/src/components/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Main = () => {
-  return (
-    <div className='h-screen text-center bg-gray-300 flex items-center justify-center font-semibold text-3xl'>Main</div>
-  )
-}
-
-export default Main
