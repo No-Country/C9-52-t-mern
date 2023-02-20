180bb2b0e810e6654b3af98efd6ca7d0670f6eaa
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-19 19:50:57 -0600

    creando el controller createComment

--- server/src/controllers/comments.controller.js
+++ server/src/controllers/comments.controller.js
@@ -4,3 +4,29 @@ const tryCatch = require('../utils/tryCatch');
 
 // models
 const Comment = require('../models/commentsModels')
+
+
+exports.createComment = tryCatch(async (req, res, next) => {
+
+  const comment = {
+    ...req.body,
+    idUser: req.user._id,
+  }
+
+  const commentNew = await Comment.create(comment);
+
+  if (!commentNew) {
+    return next(new AppError('Error al crear comentario', 400))
+  }
+
+  commentNew.save();
+  
+  return res.status(201).json({
+    status: 'success',
+    data: {
+      comment: commentNew
+    },
+    message: 'Comentario creado correctamente'
+  }).end();
+
+})
