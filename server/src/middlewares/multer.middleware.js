ed4ad509e19e2eda8be01ff1a7c2d57fb6bde343
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-23 16:51:22 -0600

    configurando el archivo multer.middleware.js

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -1,4 +1,15 @@
 const multer = require('multer');
+
+const AppError = require('../utils/AppError');
 // obtener un array de imagenes para almacenarlas en la API cloudinary
 
 const storage = multer.memoryStorage();
+
+const filterFiles = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) { 
+    cb(new AppError('No es una imagen', 400), false)
+  }
+  cb(null, true)
+}
+
+exports.upload = multer({ storage: storage, fileFilter: filterFiles });
